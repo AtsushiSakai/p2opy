779d52fd32c12dd005e8b441831891971cb77bc2
Atsushi Sakai <you@example.com>
2019-05-30 18:31:56 +0900

    first release

--- p2o2D.py
+++ p2o2D.py
@@ -1,13 +1,13 @@
 """
 
-p2o 2D optimizer in Python
+2D (x, y, yaw) pose optimizer for SLAM in Python
 
 author: Atsushi Sakai
 
 Ref:
-    - [A Compact and Portable Implementation of Graph\-based SLAM](https://www.researchgate.net/publication/321287640_A_Compact_and_Portable_Implementation_of_Graph-based_SLAM)
+- [A Compact and Portable Implementation of Graph\-based SLAM](https://www.researchgate.net/publication/321287640_A_Compact_and_Portable_Implementation_of_Graph-based_SLAM)
 
-    - [GitHub \- furo\-org/p2o: Single header 2D/3D graph\-based SLAM library](https://github.com/furo-org/p2o)
+- [GitHub \- furo\-org/p2o: Single header 2D/3D graph\-based SLAM library](https://github.com/furo-org/p2o)
 
 """
 
@@ -23,9 +23,11 @@ from scipy.sparse import linalg
 class Optimizer2D:
 
     def __init__(self):
-        self.p_lambda = 0.0
         self.verbose = False
+        self.p_lambda = 0.0
+        self.init_w = 1e10
         self.stop_thre = 1e-3
+        self.dim = 3  # state dimension
         self.robust_delta = sys.float_info.max
 
     def optimize_path(self, nodes, consts, max_iter, min_iter):
@@ -53,23 +55,21 @@ class Optimizer2D:
 
     def optimize_path_one_step(self, graph_nodes, constraints):
 
-        tripletList = []  # TODO have to change row, col data list
-        dim = 3
-        indlist = [0, 1, 2]
+        indlist = [i for i in range(self.dim)]
         numnodes = len(graph_nodes)
-        bf = np.zeros(numnodes * dim)
+        bf = np.zeros(numnodes * self.dim)
+        tripletList = TripletList()
 
         for con in constraints:
             ida = con.id1
             idb = con.id2
             assert 0 <= ida and ida < numnodes, "ida is invalid"
             assert 0 <= idb and idb < numnodes, "idb is invalid"
-            pa = graph_nodes[ida]
-            pb = graph_nodes[idb]
-
-            r, Ja, Jb = self.calc_error(pa, pb, con.t)
+            r, Ja, Jb = self.calc_error(
+                graph_nodes[ida], graph_nodes[idb], con.t)
             info = con.info_mat * self.robust_coeff(
-                r.transpose() @ con.info_mat @ r, self.robust_delta)
+                r.transpose() @ con.info_mat @ r,
+                self.robust_delta)
 
             trJaInfo = Ja.transpose() @ info
             trJaInfoJa = trJaInfo @ Ja
@@ -79,49 +79,36 @@ class Optimizer2D:
 
             for k in indlist:
                 for m in indlist:
-                    tripletList.append(
-                        (ida * dim + k, ida * dim + m, trJaInfoJa[k, m]))
-                    tripletList.append(
-                        (idb * dim + k, idb * dim + m, trJbInfoJb[k, m]))
-                    tripletList.append(
-                        (ida * dim + k, idb * dim + m, trJaInfoJb[k, m]))
-                    tripletList.append(
-                        (idb * dim + k, ida * dim + m, trJaInfoJb[m, k]))
+                    tripletList.push_back(
+                        ida * self.dim + k, ida * self.dim + m, trJaInfoJa[k, m])
+                    tripletList.push_back(
+                        idb * self.dim + k, idb * self.dim + m, trJbInfoJb[k, m])
+                    tripletList.push_back(
+                        ida * self.dim + k, idb * self.dim + m, trJaInfoJb[k, m])
+                    tripletList.push_back(
+                        idb * self.dim + k, ida * self.dim + m, trJaInfoJb[m, k])
 
-            bf[ida * dim: ida * dim + 3] += trJaInfo @ r
-            bf[idb * dim: idb * dim + 3] += trJbInfo @ r
+            bf[ida * self.dim: ida * self.dim + 3] += trJaInfo @ r
+            bf[idb * self.dim: idb * self.dim + 3] += trJbInfo @ r
 
         for k in indlist:
-            tripletList.append((k, k, 1e10))
+            tripletList.push_back(k, k, self.init_w)
 
-        for i in range(dim * numnodes):
-            tripletList.append((i, i, self.p_lambda))
+        for i in range(self.dim * numnodes):
+            tripletList.push_back(i, i, self.p_lambda)
 
-        # create sparse matrix
-        row, col, data = [], [], []
-        for t in tripletList:
-            row.append(t[0])
-            col.append(t[1])
-            data.append(t[2])
+        mat = sparse.coo_matrix((tripletList.data, (tripletList.row, tripletList.col)),
+                                shape=(numnodes * self.dim, numnodes * self.dim))
 
-        mat = sparse.coo_matrix((data, (row, col)),
-                                shape=(numnodes * dim, numnodes * dim))
-
-        x = linalg.spsolve(mat.tocsr(), bf)
-        # print(x)
-
-        # print((numnodes * dim)**2, mat.count_nonzero())
-        # print(len(tripletList))
-        # print(bf)
+        x = linalg.spsolve(mat.tocsr(), -bf)
 
         out_nodes = []
         for i in range(len(graph_nodes)):
-            u_i = i * dim
+            u_i = i * self.dim
             pos = Pose2D(
                 graph_nodes[i].x + x[u_i],
                 graph_nodes[i].y + x[u_i + 1],
-                graph_nodes[i].theta + x[u_i + 2],
-                None
+                graph_nodes[i].theta + x[u_i + 2]
             )
             out_nodes.append(pos)
 
@@ -133,49 +120,45 @@ class Optimizer2D:
         cost = 0.0
         for c in constraints:
             diff = self.error_func(nodes[c.id1], nodes[c.id2], c.t)
-            info = c.info_mat * \
-                self.robust_coeff(diff.transpose()@c.info_mat@
-                                  diff, self.robust_delta)
+            info = c.info_mat * self.robust_coeff(diff.transpose() @ c.info_mat @ diff,
+                                                  self.robust_delta)
             cost += diff.transpose() @ info @ diff
 
         return cost
 
     def robust_coeff(self, squared_error, delta):
-
         if squared_error < 0:
             return 0.0
         sqre = math.sqrt(squared_error)
         if sqre < delta:
-            return 1.0  # no effect
+            return 1.0
         return delta / sqre
 
     def error_func(self, pa, pb, t):
-        ba = self.ominus(pa, pb)
+        ba = self.calc_constraint_pose(pb, pa)
         error = np.array([ba.x - t.x,
                           ba.y - t.y,
-                          self.normalize_rad_pi_mpi(ba.theta - t.theta)])
+                          self.pi2pi(ba.theta - t.theta)])
         return error
 
-    def ominus(self, l, r):
+    def calc_constraint_pose(self, l, r):
         diff = np.array([l.x - r.x, l.y - r.y, l.theta - r.theta])
-        v = np.matmul(self.rot_mat_2d(-r.theta), diff)
-        v[2] = self.normalize_rad_pi_mpi(l.theta - r.theta)
-        return Pose2D(v[0], v[1], v[2], None)
+        v = self.rot_mat_2d(-r.theta) @ diff
+        v[2] = self.pi2pi(l.theta - r.theta)
+        return Pose2D(v[0], v[1], v[2])
 
-    def rot_mat_2d(self, th):
-        return np.array([[math.cos(th), -math.sin(th), 0.0],
-                         [math.sin(th), math.cos(th), 0.0],
+    def rot_mat_2d(self, theta):
+        return np.array([[math.cos(theta), -math.sin(theta), 0.0],
+                         [math.sin(theta), math.cos(theta), 0.0],
                          [0.0, 0.0, 1.0]
                          ])
 
     def calc_error(self, pa, pb, t):
-
         e0 = self.error_func(pa, pb, t)
         dx = pb.x - pa.x
         dy = pb.y - pa.y
         dxdt = -math.sin(pa.theta) * dx + math.cos(pa.theta) * dy
         dydt = -math.cos(pa.theta) * dx - math.sin(pa.theta) * dy
-
         Ja = np.array([[-math.cos(pa.theta), -math.sin(pa.theta), dxdt],
                        [math.sin(pa.theta), -math.cos(pa.theta), dydt],
                        [0.0, 0.0, -1.0]
@@ -184,10 +167,9 @@ class Optimizer2D:
                        [-math.sin(pa.theta), math.cos(pa.theta), 0.0],
                        [0.0, 0.0, 1.0]
                        ])
-
         return e0, Ja, Jb
 
-    def normalize_rad_pi_mpi(self, rad):
+    def pi2pi(self, rad):
 
         val = math.fmod(rad, 2.0 * math.pi)
         if val > math.pi:
@@ -198,13 +180,25 @@ class Optimizer2D:
         return val
 
 
+class TripletList:
+
+    def __init__(self):
+        self.row = []
+        self.col = []
+        self.data = []
+
+    def push_back(self, irow, icol, idata):
+        self.row.append(irow)
+        self.col.append(icol)
+        self.data.append(idata)
+
+
 class Pose2D:
 
-    def __init__(self, x, y, theta, data_id):
+    def __init__(self, x, y, theta):
         self.x = x
         self.y = y
         self.theta = theta
-        self.id = data_id
 
 
 class Constrant2D:
@@ -217,8 +211,7 @@ class Constrant2D:
 
 
 def load_data(fname):
-    nodes = []
-    consts = []
+    nodes, consts = [], []
 
     for line in open(fname):
         sline = line.split()
@@ -229,7 +222,7 @@ def load_data(fname):
             x = float(sline[2])
             y = float(sline[3])
             theta = float(sline[4])
-            nodes.append(Pose2D(x, y, theta, data_id))
+            nodes.append(Pose2D(x, y, theta))
         elif tag == "EDGE_SE2":
             id1 = int(sline[1])
             id2 = int(sline[2])
@@ -242,7 +235,7 @@ def load_data(fname):
             c4 = float(sline[9])
             c5 = float(sline[10])
             c6 = float(sline[11])
-            t = Pose2D(x, y, th, None)
+            t = Pose2D(x, y, th)
             info_mat = np.array([[c1, c2, c3],
                                  [c2, c4, c5],
                                  [c3, c5, c6]
@@ -259,6 +252,8 @@ def main():
     print("start!!")
 
     fname = "./p2o/samples/intel.g2o"
+    # fname = "./p2o/samples/manhattan3500.g2o"
+    # fname = "./p2o/samples/mit_killian.g2o"
     max_iter = 20
     min_iter = 3
     robust_thre = 1
@@ -276,14 +271,21 @@ def main():
     print("elapsed_time", time.time() - start, "sec")
 
     # plotting
+    bx, by = [], []
     for n in nodes:
-        plt.plot(n.x, n.y, ".b", label="before")
+        bx.append(n.x)
+        by.append(n.y)
+    plt.plot(bx, by, ".b", label="before")
 
+    ax, ay = [], []
     for n in final_nodes:
-        plt.plot(n.x, n.y, ".r", label="after")
+        ax.append(n.x)
+        ay.append(n.y)
+    plt.plot(ax, ay, ".r", label="after")
 
     plt.axis("equal")
     plt.grid(True)
+    plt.legend()
     plt.show()
 
     print("done!!")
